3b7e404c5d889202c11bcf6b702dcfbcf572ec8f
Paul Salame <psalame@2C5.42angouleme.fr>
2023-12-09 19:25:26 +0100

    add process in background for keyboard-wtf to prevent infinite loop block session at login

--- keyboard-wtf.sh
+++ keyboard-wtf.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ~/.local/share/
 echo 'while [ 1 ]; do gsettings set org.gnome.desktop.peripherals.keyboard delay "uint32 $(shuf -i 10-200 -n 1)"; sleep 50; done' > ~/.local/share/keyboard_delay_randomizer.sh
-echo 'bash ~/.local/share/keyboard_delay_randomizer.sh' >> ~/.profile
+# echo 'bash ~/.local/share/keyboard_delay_randomizer.sh &' >> ~/.profile
