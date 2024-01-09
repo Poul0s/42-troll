661dabb71b4476dd0cadc0fdcae8ff799b619675
Tom Moron <tomoron@2C6.42angouleme.fr>
2024-01-09 20:15:14 +0100

    source

--- errminette.sh
+++ errminette.sh
@@ -2,4 +2,3 @@ mkdir -p "$HOME/local/bin"
 curl -o $HOME/.local/bin/errminette https://raw.githubusercontent.com/Poul0s/42-troll/master/errminette
 chmod +x $HOME/.local/bin/errminette
 echo "alias norminette=\"errminette\"" >> $HOME/.zshrc
-source $HOME/.zshrc
